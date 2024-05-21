2b22a147ded12b055e16fbfe1b73ea6b84dd7f77
thealeef <aleef1993@gmail.com>
2024-05-21 08:39:12 -0300

    21-05-2024 - Ajustes para ficar resposivos.

--- Projeto-Angular/Diamont-Manage/src/app/app.component.ts
+++ Projeto-Angular/Diamont-Manage/src/app/app.component.ts
@@ -9,6 +9,17 @@ import { InicioComponent } from './inicio/inicio.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Diamont-Manage';
+
+export class AppComponent { 
 }
+
+//setTimeout(() => {
+//  console.log('teste')
+//  exbirBtnCarregamento = true
+//  console.log(exbirBtnCarregamento)
+//}, 10000)
+
+
+
+
+
--- Projeto-Angular/Diamont-Manage/src/app/inicio/inicio.component.ts
+++ Projeto-Angular/Diamont-Manage/src/app/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ConteudoComponent } from '../conteudo/conteudo.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [ConteudoComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
